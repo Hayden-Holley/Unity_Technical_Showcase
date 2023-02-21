734895bf399407d5df3958b5cffd851588f4cb89
Hayden <hayden2.holley@live.uwe.ac.uk>
2023-02-21 15:34:12 +0000

    Jump animation implemented
    Jump animation done. Playground added. The player does not move forward despite having the momentum while jumping.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator anim;
+    private Animator anim;
+    private Rigidbody rigid;
+    public float JumpForce = 500;
+    public float groundDistance = 0.3f;
+    public LayerMask whatIsGround;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         anim.SetFloat("vertical_speed", Input.GetAxis("Vertical"));
         anim.SetFloat("horizontal_speed", Input.GetAxis("Horizontal"));
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector3.up * JumpForce);
+            anim.SetTrigger("jump");
+        }
+        if (Physics.Raycast(
+            transform.position + (Vector3.up * 0.1f),
+            Vector3.down,
+            groundDistance,
+            whatIsGround))
+        {
+            anim.SetBool("grounded", true);
+            anim.applyRootMotion = true;
+        }
+        else
+        {
+            anim.SetBool("grounded", false);
+        }
     }
 }
